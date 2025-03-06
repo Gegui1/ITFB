01e5aaaa74c243449390b131d67f175e9fb08686
Gegui1 <127779171+Gegui1@users.noreply.github.com>
2025-03-06 12:41:38 +0300

    version 1

--- hw_1/main.py
+++ hw_1/main.py
@@ -1,32 +1,34 @@
-"""
-Домашнее задание №1
-Функции и структуры данных
-"""
-
-
-def power_numbers():
-    """
-    функция, которая принимает N целых чисел,
-    и возвращает список квадратов этих чисел
-    >>> power_numbers(1, 2, 5, 7)
-    <<< [1, 4, 25, 49]
-    """
-
+def power_numbers(*args):
+    return [x**2 for x in args]
 
 # filter types
 ODD = "odd"
 EVEN = "even"
 PRIME = "prime"
 
+def is_prime(n):
+    if n < 2:
+        return False
+    for i in range(2, int(n ** 0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
 
-def filter_numbers():
-    """
-    функция, которая на вход принимает список из целых чисел,
-    и возвращает только чётные/нечётные/простые числа
-    (выбор производится передачей дополнительного аргумента)
+def filter_numbers(numbers, filter_type):
+    if filter_type == ODD:
+        return list(filter(lambda x: x % 2 != 0, numbers))
+    elif filter_type == EVEN:
+        return list(filter(lambda x: x % 2 == 0, numbers))
+    elif filter_type == PRIME:
+        return list(filter(is_prime, numbers))
+    else:
+        raise ValueError("Invalid filter type")
 
-    >>> filter_numbers([1, 2, 3], ODD)
-    <<< [1, 3]
-    >>> filter_numbers([2, 3, 4, 5], EVEN)
-    <<< [2, 4]
-    """
+
+print(power_numbers(1, 2, 5, 7))
+
+numbers = [1, 2, 3, 4, 5]
+print(filter_numbers(numbers, ODD))
+print(filter_numbers(numbers, EVEN))
+print(filter_numbers(numbers, PRIME))
+
